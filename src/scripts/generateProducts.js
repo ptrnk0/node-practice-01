fda0967ba805c9d37393a97d95bad3650e71ca19
olexiy-kireyko <a.kireyko@gmail.com>
2025-02-23 10:36:54 +0200

    task2

---
+++ src/scripts/generateProducts.js
@@ -0,0 +1,13 @@
+import { createFakeProduct } from '../utils/createFakeProduct.js';
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constans/products.js';
+
+export const generateProducts = async (num) => {
+  const productsStr = await fs.readFile(PATH_DB, 'utf-8');
+  const products = JSON.parse(productsStr);
+  for (let i = 0; i < num; i++) {
+    products.push(createFakeProduct());
+  }
+  await fs.writeFile(PATH_DB, JSON.stringify(products, null, 2), 'utf-8');
+};
+generateProducts(3);
